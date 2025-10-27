dff402be45e727832c7b9c93175c62dba65477ea
Juergen Albert <j.albert@data-in-motion.biz>
2025-10-27 10:47:57 +0100

    removed temporary xsd project
    Signed-off-by: Juergen Albert <j.albert@data-in-motion.biz>

--- org.eclipse.fennec.model.atlas.rest.xsdschema/src/org/eclipse/fennec/model/atlas/rest/xsdschema/XSDSchemaMessageBodyReaderWriter.java
+++ org.eclipse.fennec.model.atlas.rest.xsdschema/src/org/eclipse/fennec/model/atlas/rest/xsdschema/XSDSchemaMessageBodyReaderWriter.java
@@ -19,19 +19,17 @@ import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.fennec.codec.options.CodecModelInfoOptions;
-import org.eclipse.fennec.codec.options.CodecModuleOptions;
-import org.eclipse.fennec.codec.options.CodecResourceOptions;
-import org.gecko.emf.osgi.constants.EMFNamespaces;
+import org.eclipse.emf.ecore.util.BasicExtendedMetaData;
+import org.eclipse.xsd.XSDSchema;
+import org.eclipse.xsd.ecore.EcoreXMLSchemaBuilder;
+import org.eclipse.xsd.ecore.XSDEcoreBuilder;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceScope;
@@ -50,12 +48,12 @@ import org.w3c.dom.UserDataHandler;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.ext.MessageBodyReader;
 import jakarta.ws.rs.ext.MessageBodyWriter;
 import jakarta.ws.rs.ext.Provider;
-import org.eclipse.xsd.ecore.EcoreXMLSchemaBuilder;
 
 /**
  * 
@@ -96,233 +94,11 @@ public class XSDSchemaMessageBodyReaderWriter implements MessageBodyReader<EPack
       MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream)
       throws IOException, WebApplicationException {
     EcoreXMLSchemaBuilder schemaBuilder = new EcoreXMLSchemaBuilder();
-    Node node = new Node() {
-      
-      @Override
-      public Object setUserData(String key, Object data, UserDataHandler handler) {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public void setTextContent(String textContent) throws DOMException {
-        // TODO Auto-generated method stub
-        
-      }
-      
-      @Override
-      public void setPrefix(String prefix) throws DOMException {
-        // TODO Auto-generated method stub
-        
-      }
-      
-      @Override
-      public void setNodeValue(String nodeValue) throws DOMException {
-        // TODO Auto-generated method stub
-        
-      }
-      
-      @Override
-      public Node replaceChild(Node newChild, Node oldChild) throws DOMException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public Node removeChild(Node oldChild) throws DOMException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public void normalize() {
-        // TODO Auto-generated method stub
-        
-      }
-      
-      @Override
-      public String lookupPrefix(String namespaceURI) {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public String lookupNamespaceURI(String prefix) {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public boolean isSupported(String feature, String version) {
-        // TODO Auto-generated method stub
-        return false;
-      }
-      
-      @Override
-      public boolean isSameNode(Node other) {
-        // TODO Auto-generated method stub
-        return false;
-      }
-      
-      @Override
-      public boolean isEqualNode(Node arg) {
-        // TODO Auto-generated method stub
-        return false;
-      }
-      
-      @Override
-      public boolean isDefaultNamespace(String namespaceURI) {
-        // TODO Auto-generated method stub
-        return false;
-      }
-      
-      @Override
-      public Node insertBefore(Node newChild, Node refChild) throws DOMException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public boolean hasChildNodes() {
-        // TODO Auto-generated method stub
-        return false;
-      }
-      
-      @Override
-      public boolean hasAttributes() {
-        // TODO Auto-generated method stub
-        return false;
-      }
-      
-      @Override
-      public Object getUserData(String key) {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public String getTextContent() throws DOMException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public Node getPreviousSibling() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public String getPrefix() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public Node getParentNode() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public Document getOwnerDocument() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public String getNodeValue() throws DOMException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public short getNodeType() {
-        // TODO Auto-generated method stub
-        return 0;
-      }
-      
-      @Override
-      public String getNodeName() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public Node getNextSibling() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public String getNamespaceURI() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public String getLocalName() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public Node getLastChild() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public Node getFirstChild() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public Object getFeature(String feature, String version) {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public NodeList getChildNodes() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public String getBaseURI() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public NamedNodeMap getAttributes() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public short compareDocumentPosition(Node other) throws DOMException {
-        // TODO Auto-generated method stub
-        return 0;
-      }
-      
-      @Override
-      public Node cloneNode(boolean deep) {
-        // TODO Auto-generated method stub
-        return null;
-      }
-      
-      @Override
-      public Node appendChild(Node newChild) throws DOMException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-    };
+   
     Collection<EObject> collection = schemaBuilder.generate(t);
-    
-    Resource resource = resourceSet.createResource(URI.createURI("temp.xsd"));
+    String fileName = t.getName() + ".xsd";
+    httpHeaders.put(HttpHeaders.CONTENT_DISPOSITION, List.of("attachment; filename=" + fileName));
+    Resource resource = resourceSet.createResource(URI.createURI(fileName));
     resource.getContents().addAll(collection);
     resource.save(entityStream, null);
     resource.getContents().clear();
@@ -346,9 +122,15 @@ public class XSDSchemaMessageBodyReaderWriter implements MessageBodyReader<EPack
   public EPackage readFrom(Class<EPackage> type, Type genericType, Annotation[] annotations, MediaType mediaType,
       MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
       throws IOException, WebApplicationException {
-    Resource resource = resourceSet.createResource(URI.createURI("temp.json"), "application/json");
-    EPackage ePackage = resource.getContents().isEmpty() ? null : (EPackage) resource.getContents().remove(0);
-    return ePackage;
+    Resource resource = resourceSet.createResource(URI.createURI("temp.xsd"));
+    XSDSchema schema = resource.getContents().isEmpty() ? null : (XSDSchema) resource.getContents().remove(0);
+    if(schema == null) {
+      return null;
+    }
+    XSDEcoreBuilder ecoreBuilder = new XSDEcoreBuilder(new BasicExtendedMetaData(resourceSet.getPackageRegistry()));
+    ecoreBuilder.generate(schema);
+    Collection<EPackage> values = ecoreBuilder.getTargetNamespaceToEPackageMap().values();
+    return values.iterator().next();
   }
 
 }
