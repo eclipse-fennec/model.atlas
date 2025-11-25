03c8466a78022d405e368ea7573b271211082734
Juergen Albert <j.albert@data-in-motion.biz>
2025-11-25 16:39:16 +0100

    adds license header
    Signed-off-by: Juergen Albert <j.albert@data-in-motion.biz>

--- org.eclipse.fennec.model.atlas.management.apicurio.model/src/org/eclipse/fennec/model/atlas/mgmt/mgmtapicurio/MgmtApicurioPackage.java
+++ org.eclipse.fennec.model.atlas.management.apicurio.model/src/org/eclipse/fennec/model/atlas/mgmt/mgmtapicurio/MgmtApicurioPackage.java
@@ -1,4 +1,15 @@
 /*
+ * Copyright (c) 2012 - 2025 Data In Motion and others.
+ * All rights reserved.
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
  */
 package org.eclipse.fennec.model.atlas.mgmt.mgmtapicurio;
 
@@ -27,7 +38,7 @@ import org.osgi.annotation.versioning.ProviderType;
  * @see org.eclipse.fennec.model.atlas.mgmt.mgmtapicurio.MgmtApicurioFactory
  * @model kind="package"
  *        annotation="Version value='1.0'"
- *        annotation="http://www.eclipse.org/emf/2002/GenModel complianceLevel='17.0' oSGiCompatible='true' basePackage='org.eclipse.fennec.model.atlas.mgmt' resource='XMI'"
+ *        annotation="http://www.eclipse.org/emf/2002/GenModel complianceLevel='17.0' oSGiCompatible='true' basePackage='org.eclipse.fennec.model.atlas.mgmt' resource='XMI' copyrightText='Copyright (c) 2012 - 2025 Data In Motion and others.\nAll rights reserved.\n\nThis program and the accompanying materials are made\navailable under the terms of the Eclipse Public License 2.0\nwhich is available at https://www.eclipse.org/legal/epl-2.0/\n\nSPDX-License-Identifier: EPL-2.0\n\nContributors:\n    Data In Motion - initial API and implementation'"
  * @generated
  */
 @ProviderType
--- org.eclipse.fennec.model.atlas.management.apicurio.model/src/org/eclipse/fennec/model/atlas/mgmt/mgmtapicurio/impl/MgmtApicurioPackageImpl.java
+++ org.eclipse.fennec.model.atlas.management.apicurio.model/src/org/eclipse/fennec/model/atlas/mgmt/mgmtapicurio/impl/MgmtApicurioPackageImpl.java
@@ -1,4 +1,15 @@
 /**
+ * Copyright (c) 2012 - 2025 Data In Motion and others.
+ * All rights reserved.
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
  */
 package org.eclipse.fennec.model.atlas.mgmt.mgmtapicurio.impl;
 
@@ -907,7 +918,8 @@ public class MgmtApicurioPackageImpl extends EPackageImpl implements MgmtApicuri
 			   "complianceLevel", "17.0",
 			   "oSGiCompatible", "true",
 			   "basePackage", "org.eclipse.fennec.model.atlas.mgmt",
-			   "resource", "XMI"
+			   "resource", "XMI",
+			   "copyrightText", "Copyright (c) 2012 - 2025 Data In Motion and others.\nAll rights reserved.\n\nThis program and the accompanying materials are made\navailable under the terms of the Eclipse Public License 2.0\nwhich is available at https://www.eclipse.org/legal/epl-2.0/\n\nSPDX-License-Identifier: EPL-2.0\n\nContributors:\n    Data In Motion - initial API and implementation"
 		   });
 		addAnnotation
 		  (createArtifactEClass,
