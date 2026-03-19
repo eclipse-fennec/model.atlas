ea85e4e64e9a9ab2eadcc18f4199895281ff974a
Guido Grune <g.grune@datainmotion.com>
2026-03-19 10:55:37 +0100

    jena initial config
    Signed-off-by: Guido Grune <g.grune@datainmotion.com>

--- build-jena-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-./gradlew :org.eclipse.fennec.model.atlas.runtime.config.local.jena:build
-
-cp org.eclipse.fennec.model.atlas.runtime.config.local.jena/generated/org.eclipse.fennec.model.atlas.runtime.config.local.jena.jar \
-   docker/dockercompose/
