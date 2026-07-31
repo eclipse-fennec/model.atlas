29bda04425b8d77aabdc983e3424446d7f83fafa
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2026-07-31 10:57:41 +0200

    adjuste copyright text in datagen models
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

--- org.eclipse.fennec.model.atlas.datagen.example.model/src/org/eclipse/fennec/model/atlas/datagen/example/model/dge/impl/DGPackageImpl.java
+++ org.eclipse.fennec.model.atlas.datagen.example.model/src/org/eclipse/fennec/model/atlas/datagen/example/model/dge/impl/DGPackageImpl.java
@@ -1,4 +1,15 @@
 /**
+ * Copyright (c) 2012 - 2026 Data In Motion and others.
+ * All rights reserved.
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
  */
 package org.eclipse.fennec.model.atlas.datagen.example.model.dge.impl;
 
@@ -507,7 +518,8 @@ public class DGPackageImpl extends EPackageImpl implements DGPackage {
 			   "complianceLevel", "17.0",
 			   "oSGiCompatible", "true",
 			   "basePackage", "org.eclipse.fennec.model.atlas.datagen.example.model",
-			   "resource", "XMI"
+			   "resource", "XMI",
+			   "copyrightText", "Copyright (c) 2012 - 2026 Data In Motion and others.\nAll rights reserved.\n\nThis program and the accompanying materials are made\navailable under the terms of the Eclipse Public License 2.0\nwhich is available at https://www.eclipse.org/legal/epl-2.0/\n\nSPDX-License-Identifier: EPL-2.0\n\nContributors:\n    Data In Motion - initial API and implementation"
 		   });
 	}
 
--- org.eclipse.fennec.model.atlas.datagen.model/src/org/eclipse/fennec/model/atlas/datagen/model/datagen/DatagenPackage.java
+++ org.eclipse.fennec.model.atlas.datagen.model/src/org/eclipse/fennec/model/atlas/datagen/model/datagen/DatagenPackage.java
@@ -1,4 +1,15 @@
 /*
+ * Copyright (c) 2012 - 2026 Data In Motion and others.
+ * All rights reserved.
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
  */
 package org.eclipse.fennec.model.atlas.datagen.model.datagen;
 
@@ -29,7 +40,7 @@ import org.osgi.annotation.versioning.ProviderType;
  * <!-- end-model-doc -->
  * @see org.eclipse.fennec.model.atlas.datagen.model.datagen.DatagenFactory
  * @model kind="package"
- *        annotation="http://www.eclipse.org/emf/2002/GenModel oSGiCompatible='true' basePackage='org.eclipse.fennec.model.atlas.datagen.model' copyrightText='Copyright (c) 2026 Contributors to the Eclipse Foundation.\n\nThis program and the accompanying materials are made\navailable under the terms of the Eclipse Public License 2.0\nwhich is available at https://www.eclipse.org/legal/epl-2.0/\n\nSPDX-License-Identifier: EPL-2.0\n\nContributors:\n  Data In Motion Consulting - initial implementation' resource='XMI' fileExtensions='datagen'"
+ *        annotation="http://www.eclipse.org/emf/2002/GenModel oSGiCompatible='true' basePackage='org.eclipse.fennec.model.atlas.datagen.model' copyrightText='Copyright (c) 2012 - 2026 Data In Motion and others.\nAll rights reserved.\n\nThis program and the accompanying materials are made\navailable under the terms of the Eclipse Public License 2.0\nwhich is available at https://www.eclipse.org/legal/epl-2.0/\n\nSPDX-License-Identifier: EPL-2.0\n\nContributors:\n    Data In Motion - initial API and implementation' resource='XMI' fileExtensions='datagen'"
  * @generated
  */
 @ProviderType
